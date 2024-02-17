b1bc7d1b58e4d72192233a0cea01a667252639b0
Jakub Vsetecka <jakub01vsetecka@seznam.cz>
2024-02-17 14:04:43 +0100

    add: parsing util

--- parse_utils.py
+++ parse_utils.py
@@ -13,6 +13,7 @@ class XMLGenerator:
         self.instruction_count = 0  # Initialize instruction counter
     
     def add_instruction(self, opcode, *args):
+        return #TODO
         self.instruction_count += 1  # Increment instruction counter
         order = self.instruction_count
         instruction = ET.SubElement(self.root, "instruction", order=str(order), opcode=opcode.upper())
@@ -21,7 +22,7 @@ class XMLGenerator:
             arg_elem.text = self._format_value(arg_type, arg_value)
     
     def _format_value(self, arg_type, arg_value):
-        if arg_type == "var":
+        if arg_type == ArgumentInInst.VAR.value:
             return arg_value.upper()  # Memory frame in uppercase
         elif arg_type == "string":
             return arg_value.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
@@ -36,40 +37,64 @@ class XMLGenerator:
     def get_instruction_count(self):
         return self.instruction_count
 
+class ArgumentType(Enum):
+    INT = r'\d+'
+    BOOL = r'(true|false)'
+    STRING = r'[^#\s]+'
+    NIL = r'nil@nil'
+    LABEL = r'(\w+)'
+    VAR = r'(GF|TF|LF)@(\w+)'
+
+class ArgumentInInst(Enum):
+    VAR = ArgumentType.VAR.value
+    SYMB = "|".join([
+        ArgumentType.INT.value,
+        ArgumentType.BOOL.value,
+        ArgumentType.STRING.value,
+        ArgumentType.NIL.value
+    ])
+    LABEL = ArgumentType.LABEL.value
+    TYPE = "|".join([
+        ArgumentType.STRING.value,
+        ArgumentType.NIL.value,
+        ArgumentType.BOOL.value,
+        ArgumentType.INT.value
+    ])
+
 class Instruction(Enum):
-    MOVE = ["var", "symb"],
-    CREATEFRAME = [],
-    PUSHFRAME = [],
-    POPFRAME = [],
-    DEFVAR = ["var"],
-    CALL = ["label"],
-    RETURN = [],
-    PUSHS = ["symb"],
-    POPS = ["var"],
-    ADD = ["var", "symb", "symb"],
-    SUB = ["var", "symb", "symb"],
-    MUL = ["var", "symb", "symb"],
-    IDIV = ["var", "symb", "symb"],
-    LT = ["var", "symb", "symb"],
-    GT = ["var", "symb", "symb"],
-    EQ = ["var", "symb", "symb"],
-    AND = ["var", "symb", "symb"],
-    OR = ["var", "symb", "symb"],
-    NOT = ["var", "symb"],
-    INT2CHAR = ["var", "symb"],
-    STRI2INT = ["var", "symb", "symb"],
-    READ = ["var", "type"],
-    WRITE = ["symb"],
-    CONCAT = ["var", "symb", "symb"],
-    STRLEN = ["var", "symb"],
-    GETCHAR = ["var", "symb", "symb"],
-    SETCHAR = ["var", "symb", "symb"],
-    TYPE = ["var", "symb"],
-    LABEL = ["label"],
-    JUMP = ["label"],
-    JUMPIFEQ = ["label", "symb", "symb"],
-    JUMPIFNEQ = ["label", "symb", "symb"],
-    EXIT = ["symb"]
+    MOVE = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value]
+    CREATEFRAME = []
+    PUSHFRAME = []
+    POPFRAME = []
+    DEFVAR = [ArgumentInInst.VAR.value]
+    CALL = [ArgumentInInst.LABEL.value]
+    RETURN = []
+    PUSHS = [ArgumentInInst.SYMB.value]
+    POPS = [ArgumentInInst.VAR.value]
+    ADD = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    SUB = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    MUL = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    IDIV = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    LT = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    GT = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    EQ = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    AND = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    OR = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    NOT = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value]
+    INT2CHAR = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value]
+    STRI2INT = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    READ = [ArgumentInInst.VAR.value, ArgumentInInst.TYPE.value]
+    WRITE = [ArgumentInInst.SYMB.value]
+    CONCAT = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    STRLEN = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value]
+    GETCHAR = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    SETCHAR = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    TYPE = [ArgumentInInst.VAR.value, ArgumentInInst.SYMB.value]
+    LABEL = [ArgumentInInst.LABEL.value]
+    JUMP = [ArgumentInInst.LABEL.value]
+    JUMPIFEQ = [ArgumentInInst.LABEL.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    JUMPIFNEQ = [ArgumentInInst.LABEL.value, ArgumentInInst.SYMB.value, ArgumentInInst.SYMB.value]
+    EXIT = [ArgumentInInst.SYMB.value]
 
 
 """
@@ -80,33 +105,32 @@ class CodeParser:
         self.xml_generator = XMLGenerator()
         
     def parse_argument(self, arg, type):
-        match type:
-            case "var":
-                return arg
-            case "symb":
-                return arg
-            case "label":
-                return arg
-            case "type":
-                return arg
-        pass
+        print(arg, type)
+        matches = [match.group(0) for match in re.finditer(type, arg)]
+        print(matches)
+        if len(matches) != 1: #TODO
+            sys.exit(21)
+        return matches[0]
 
-    def parse_arguments(self, args, types):
-        for arg, type in zip(args, types):
-            yield self.parse_argument(arg, type)
-            
+    def parse_arguments(self, types, args):
+        return [self.parse_argument(args[i], types[i]) for i in range(len(types))]
 
     def parse_line(self, line):
         # Example parsing logic - you would replace this with actual parsing code
         parts = line.split()
         print(parts)
         opcode = parts[0]
-        types = Instruction[opcode].value
 
+        types = Instruction[opcode].value
+        print(types)
         if len(parts) - 1 != len(types):
+            print(f"Invalid number of arguments for instruction {opcode}")
+            print(f"Expected {len(types)} arguments, got {len(parts) - 1}")
+            print(types, parts)
             sys.exit(21)
 
-        args = self.parse_arguments(parts[1:], types)
+        args = self.parse_arguments(types, parts[1:])
+        print(f"\033[34m{args}\033[0m")
         self.xml_generator.add_instruction(opcode, *args)
 
     def remove_header(self, code):
@@ -120,7 +144,11 @@ class CodeParser:
         return re.sub(r'#.*', '', code)
     
     def remove_empty_lines(self, code):
-        return re.sub(r'(?m)^\s*\n|\n\s*$', '', code)
+        # Remove lines that consist only of whitespace
+        cleaned_code = re.sub(r'(?m)^\s*\n', '', code)
+        # Remove trailing whitespace at the end of the string, if present, without removing the final newline
+        cleaned_code = re.sub(r'\n\s*$', '\n', cleaned_code)
+        return cleaned_code
         
     def parse_code(self, code):
         code = self.remove_comments(code)
